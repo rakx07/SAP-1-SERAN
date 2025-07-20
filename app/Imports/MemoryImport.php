39cee3c396f48be601f75dbfbc6fb5d3ea6e6e67
Janry S <janryseran069407@gmail.com>
2025-07-20 21:24:52 +0800

    fixed excel upload error

--- app/Imports/MemoryImport.php
+++ app/Imports/MemoryImport.php
@@ -9,11 +9,11 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 class MemoryImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
-{
-    return new Memory([
-        'address' => str_pad(decbin($row[0]), 4, '0', STR_PAD_LEFT),
-        'instruction' => $row[1],
-        'value' => $row[2], // <- Make sure your Excel has this
-    ]);
-}
+    {
+        return new Memory([
+            'address' => str_pad($row['address'], 4, '0', STR_PAD_LEFT),
+            'instruction' => $row['instruction'],
+            'value' => $row['value'] ?? null, // allow null if column is empty
+        ]);
+    }
 }
